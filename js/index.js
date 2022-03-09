556bf158e2f8076ba4276dabe2b1f9fcae28f48b
Sammonnay-Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2022-03-09 18:35:22 +0530

    coming soon animation trial(9-3-2022)

--- js/index.js
+++ js/index.js
@@ -33,4 +33,42 @@ for(i=1;i<4;i++){
 	})
 
 }
+var z=0;
+// while(z<1){
+const text1 =  baffle(".first");
+text1.set({
+  characters: '░▒░ ░██░> ████▓ >█> ░/█>█ ██░░ █<▒ ▓██░ ░/░▒',
+        speed: 120
+});
+text1.start();
+text1.reveal(1000);
+text1.start()
 
+const text2 =  baffle(".second");
+text2.set({
+  characters: '░▒░ ░██░> ████▓ >█> ░/█>█ ██░░ █<▒ ▓██░ ░/░▒',
+        speed: 120
+});
+text2.start();
+text2.reveal(1000, 3000);
+text2.start();
+
+const text3 =  baffle(".third");
+text3.set({
+  characters: '░▒░ ░██░> ████▓ >█> ░/█>█ ██░░ █<▒ ▓██░ ░/░▒',
+        speed: 120
+});
+text3.start();
+text3.reveal(1000, 5000);
+text3.start();
+
+
+const text4 =  baffle(".fourth");
+text4.set({
+  characters: '░▒░ ░██░> ████▓ >█> ░/█>█ ██░░ █<▒ ▓██░ ░/░▒',
+        speed: 120
+});
+text4.start();
+text4.reveal(1000, 8000);
+text4.start();
+// }
