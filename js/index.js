acf205b82748928fd7f904b98a1dd6df430ff6d7
Sammonnay-Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2022-03-21 17:51:52 +0530

    "hologram"

--- js/index.js
+++ js/index.js
@@ -1,43 +1,21 @@
-$(document).ready(function(){
-	$('.toggle-box').click(function(){
-        
-		$(this).toggleClass('open');
-        $(".navlist").toggleClass("show");
-	});
-
+$(document).ready(function () {
+  $(".toggle-box").click(function () {
+    $(this).toggleClass("open");
+    $(".navlist").toggleClass("show");
+  });
 });
 
 var portfolioExperiment = document.querySelectorAll(".nav-link");
 var i;
-console.log(portfolioExperiment);
 
-for(i=1;i<4;i++){
-	
-	portfolioExperiment[i].addEventListener("mouseover",function(e){
-		var j;
-		for(j=0;j<4;j++){
-			if(portfolioExperiment[j]==e.path[1]);
-			{
-				console.log(e.path[1] ," " +e.path[1].children[0]);
-				console.log("found");
-				break;
-			}
-		}
-		
-		
-		// var j= portfolioExperiment.indexOf(x);
-		// var c=j-1;
-		// console.log(c);
-		// console.log(portfolioExperiment[c]);
-		// portfolioExperiment[c].classList.remove("effect");
-	})
-
-}
-var text= baffle(".data");
+var text = baffle(".data");
 
 text.set({
-    characters: "█▓█ ▒░/▒░ █░▒▓/ █▒▒ ▓▒▓/█ ░█▒/ ▒▓░ █<░▒ ▓/░>",
-    speed: 120
+  characters: "█▓█ ▒░/▒░ █░▒▓/ █▒▒ ▓▒▓/█ ░█▒/ ▒▓░ █<░▒ ▓/░>",
+  speed: 120,
 });
 text.start();
 text.reveal(3000);
+
+
+
