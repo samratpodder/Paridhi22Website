35357978a1137e623ce84382c47da319f40c6098
samratpodder <iamsamrat@outlook.com>
2022-04-18 22:11:06 +0530

    rebasing without rebasing

---
+++ js/mainevent.js
@@ -0,0 +1,323 @@
+//events data
+
+const domainDeatails =[
+    {
+      id : 'Robotics',
+      idNo:1,
+      domainName:'ROBOTICS EVENT',
+      backgroundImgName:'https://images.unsplash.com/photo-1553095066-5014bc7b7f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbCUyMGJhY2tncm91bmR8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60',
+      eventDetails: [{
+          eventName:'HOT-WHEELS(On Spot(RACE))',
+          eventdescription:'Hot-wheel need unceremonious players and a hardcore elite in  order to exists. The climax of competitive scene players with a goal shoot for Players get to race, crash and speed their way to victory with the winner on spot. But beware, it is not a normal Game, it is the epic race of bot. Tune up your skills so you can go hard or just go home.',
+          coordinator:'Soumajit Mitra',
+          coordinatorNo:'+91-9007809587',
+          eventurl:'../html/manual/hotwheels.html',
+      },
+      {
+        eventName:'ROAD-TO-HEAVEN(Roborace)',
+        eventdescription:"You are trapped inside the eternal purgatory and only way out of this hell is to reach the gates of heaven. You have to race it out, crash and clear the obstacles as soon as possible on the \"Road to Heaven\" To gets the chance to finally have peace.",
+        coordinator:'Rishov Banerjee',
+        coordinatorNo:'+91-8337080341',
+        eventurl:'../html/manual/road-to-heaven.html',
+    },
+    {
+        eventName:'ROBO-CLASH(Robowar)',
+        eventdescription:"Robo-clash is one of most exciting events of Paridhi in which two participants fight with their bots. In this event the participants have to design a robot using great technical effort and customized tools to fight with their opponents and to prove your worth in the battle field.",
+        coordinator:'Diptargha Khasnobis',
+        coordinatorNo:'+91-8240544948',
+        eventurl:'../html/manual/Robo-Clash.html',
+    },
+    {
+        eventName:'ROCKET LEAGUE(Robosoccer)',
+        eventdescription:"Battlefield is not the only field you are challenged, Soccer being the ultimate soul game for us, it's the soccer field too, and who can play it better than the engineers, in their own sophisticated way. Engineers have the knack of fusing technology with everything, and sports is no exception to that, so to celebrate this very spirit in a whole new dimension, we hail this event known as Rocket league.",
+        coordinator:'Soumyadeep Majumder',
+        coordinatorNo:'+91-7980333373',
+        eventurl:'../html/manual/rocket-league.html',
+    },
+    {
+        eventName:'SQUID GAME(on-spot(Time Based))',
+        eventdescription:"Wanna have some actions with your bot and make instant money? We bring to you the \"Squid Game\". In this first of it\'s kind robotics event you get to race, crash and speed your way to victory with the winner getting exploits on spot. But beware, it isn\’t a normal \"Game\", it\’s the epic \"Battle of Life\". Tune up your skills so you can go hard or just go home.",
+        coordinator:'Rishov Banerjee',
+        coordinatorNo:'+91-8337080341',
+        eventurl:'../html/manual/squid-game.html',
+    },
+    {
+        eventName:'VENGEANCE(Maze with Pick-drop mechanism)',
+        eventdescription:"Gotham City is in danger as the Riddler wreaks havoc, Jim Gordon needs your help to activate the Bat-signal. “Help him activate the bat signal by putting the right pieces together...!!” The city needs you to help Batman fight the Riddler and save the city. Hurry because the time is running up from your hand.",
+        coordinator:'Soumajit Mitra',
+        coordinatorNo:'+91-9007809587',
+        eventurl:'../html/manual/vengeance.html',
+    }
+],
+    },
+    {
+        id : 'Gaming',
+        idNo:2,
+        domainName:'GAMING EVENT',
+        backgroundImgName:'pngegg.png',
+        eventDetails: [
+            {
+                eventName:'VALORANT',
+                eventdescription:"Valorant Tournament",
+                coordinator:'Parshan Banerjee',
+                coordinatorNo:'+91-7003405284',
+                eventurl:'../html/gaming/valorant.html',
+            },
+            {
+            eventName:'BGMI',
+            eventdescription:"BGMI Touranament",
+            coordinator:'Parshan Banerjee',
+            coordinatorNo:'+91-7003405284',
+            eventurl:'../html/gaming/bgmi.html',
+        },
+        {
+          eventName:'CSGO',
+          eventdescription:"CSGO Tournament",
+          coordinator:'Parshan Banerjee',
+          coordinatorNo:'+91-7003405284',
+          eventurl:'../html/gaming/CSGO.html',
+      },
+      {
+        eventName:'FIFA',
+        eventdescription:"FIFA Tournament",
+        coordinator:'Parshan Banerjee',
+        coordinatorNo:'+91-7003405284',
+        eventurl:'../html/gaming/fifa.html',
+    },
+    {
+        eventName:'NFS',
+        eventdescription:"NFS Tournament",
+        coordinator:'Akash Halder',
+        coordinatorNo:'+91-8336059129',
+        eventurl:'../html/gaming/nfs.html',
+    }],
+    },
+      {
+        id : 'Electrical',
+        idNo:3,
+        domainName:'Eletrical',
+        backgroundImgName:'pngegg.png',
+        eventDetails: [{
+            eventName:'Electro-Spark',
+            eventdescription:"A three rounds event consisting of word search, circuit completion and riddle number combination.",
+            coordinator:'Ankur Mondal',
+            coordinatorNo:'+91-8240317183',
+            eventurl:'../html/electrical/electro-spark.html',
+        },
+        {
+          eventName:'Fuzlon',
+          eventdescription:"A knockout event where participants have to battle out with opponent by switching off the maximum number of lights and scoring the maximum points.",
+          coordinator:'Vartika Sultania',
+          coordinatorNo:'+91-9681065195',
+          eventurl:'../html/electrical/fuzlon.html',
+      }],
+      },
+      {
+        id : 'Coding',
+        idNo:4,
+        domainName:'Coding',
+        backgroundImgName:'pngegg.png',
+        eventDetails: [{
+            eventName:'Clash of Coders',
+            eventdescription:"Are you a seasoned coder ? Or just starting your journey into the world of Sport Programming. You are exploring just the perfect event. A 2 hour long battle involving problem solving and brain wrenching bending of 1s & 0s. Fight your way up the leaderboard by solving most and fast;and Stand a Chance to win exciting Prizes & Goodiesi.",
+            coordinator:'Deeptaneel Dey',
+            coordinatorNo:'+91-7003297265',
+            eventurl:'../html/coding/clash-of-coders.html',
+        },
+        {
+          eventName:'Code Hashers',
+          eventdescription:"A strong developer will be in demand for the foreseeable future!And Coding is the basic requirement to develop yourself.Here we present an event, Code Hashers for you guys to showcase your programming skills.It is all year coding competition.It's all about understanding with your team mate.It's 2V2 competition.It contains team of 2 members.It's all about team work!! Here you can solve interesting Programming and earn cash prizes!So come with your partner and showcase your talents in Paridhi.",
+          coordinator:'Srutarshi Ghosh',
+          coordinatorNo:'+91-7980602909',
+          eventurl:'../html/coding/codehashers.html',
+      },
+      {
+        eventName:'Code Sprint',
+        eventdescription:"Megatronix presents an online coding event “Code Sprint”.",
+        coordinator:'Deeptaneel Dey',
+        coordinatorNo:'+91-7003297265',
+        eventurl:'../html/coding/codesprint.html',
+    },
+    {
+        eventName:'Web [.]In',
+        eventdescription:" We have all thought of developing a designer website where all the details including as small as contacts to as large as a biography is present which is just a web address away to reaching the mass.So this is your chance to participate in Web.[IN] and develop your very OWN modish and edgy websites. This competition provides you the best platform to showcase your web dev skills and being unique can be rewarding as there are great cash prizes to win.  So what's the wait, join the event and become a part of Paridhi 2022",
+        coordinator:'Subinoy Mukherjee',
+        coordinatorNo:'+91-8210578027',
+        eventurl:'../html/coding/webin.html',
+    }],
+      },
+      {
+        id : 'Civil',
+        idNo:5,
+        domainName:'Civil',
+        backgroundImgName:'pngegg.png',
+        eventDetails: [{
+            eventName:'Mega Arch',
+            eventdescription:"Wanna get some real life experience with various bridge then surly be a part of MEGA ARCH where you can make bridge with bricks and sand Paridhi 2022 brings to you Chances to win your pocket money just by making bridges that you all made in your childhood with cards. THIS EVENT IS OPEN FOR ALL.",
+            coordinator:'Anirban Ghosh',
+            coordinatorNo:'+91-7003165530',
+            eventurl:'../html/civil/megaarch.html',
+        },
+        {
+          eventName:'Setu Bandhan',
+          eventdescription:"Aspiring to be a Civil engineer or trying to feel like one??? Then here is your chance Yes you can make your own bridge with only ice cream sticks and glue Paridhi 2022 brings to you another chance to test your skills, so what are you waiting for?? Pull up your socks and make the best bridge so that it can take the load of your fattest friend. THIS EVENT IS OPEN FOR ALL.",
+          coordinator:'Anirban Ghosh',
+          coordinatorNo:'+91-7003165530',
+          eventurl:'../html/civil/setu-bandhan.html',
+      },
+      {
+        eventName:'Track-o-Treasure',
+        eventdescription:"If solving tricky yet easy sums in a minute is your hobby then come and join us in Paridhi 2022 and be a part of Track-o-treasure. All you need is a measuring tape and a mobile compass that will be provided. Just come and play to discover what's hidden for you at the end.",
+        coordinator:'Anirban Ghosh',
+        coordinatorNo:'+91-7003165530',
+        eventurl:'../html/civil/Track-o-treasure.html',
+    }],
+      },
+      {
+        id : 'Autonomous',
+        idNo:6,
+        domainName:'Autonomous',
+        backgroundImgName:'pngegg.png',
+        eventDetails: [{
+            eventName:'DODGE IT',
+            eventdescription:`Build a fully autonomous bot capable of avoiding certain obstacles
+            placed on the track while tracing white/black lines on a black/white
+            surface to reach the destination. The bot has to cover a specified
+            track within the allotted time.
+            The bot that takes the minimum time to complete the whole
+            track avoiding all the obstacles i.e. with the highest score will
+            be declared as the winner of the event.`,
+            coordinator:'Kunal Agarwal',
+            coordinatorNo:'+91-9163470021',
+            eventurl:'../html/autonomous/DodgeIT.html',
+        },
+        {
+          eventName:'LINE-O-CHASE',
+          eventdescription:`Build a fully Autonomous Bot capable of traversing
+          white/black lines on a black / white surface to reach the
+          destination. The bot has to cover a specified track within
+          the allotted time.
+          The bot that takes the minimum time to complete the
+          track, while accurately tracking it, i.e. with the highest
+          score will be declared as the winner of the event.`,
+          coordinator:'Amit Kumar Shaw',
+          coordinatorNo:'+91-6291830437',
+          eventurl:'../html/autonomous/LineOChase.html',
+      },
+    ],
+      }]
+
+// dom target
+
+const domainHeading = document.querySelector('#domainHeading')
+const eventsDetail = document.querySelector('.events-details')
+const eventlistUl = document.querySelector('#event-ul')
+const detailsHeading= document.querySelector('#detailsHeading')
+const backgroundImg = document.querySelector('.domain-backgroundimg')
+const domainList = document.querySelectorAll('.domain-div')
+
+var idnum=0;
+var prevDomain="";
+var domainFlag=true;
+var firstFlag = true;
+
+function getRoboticsEvent(domainDetail){
+    return domainDetail.domainName === "ROBOTICS EVENT";
+}
+
+if(eventlistUl.children.length == 0){
+
+    var roboticsDomain = domainDeatails.filter((domainDetail) => domainDetail.domainName === "ROBOTICS EVENT");
+    roboticsDomain[0].eventDetails.map((data) => {
+            const newlist = document.createElement('li')
+            newlist.classList.add('event-eachdeatils')
+            newlist.innerHTML=`
+                            <li class="event-eachdeatils">
+                                <div class="details-heading" >${data.eventName}</div>
+                                <div class="details-description">${data.eventdescription}</div>
+                                <div class="coordinator">Coordinator: <span id="coordinator-name">${data.coordinator} (${data.coordinatorNo})</span></div>
+                                <div class="event-detailsbtndiv">
+                                <button type="button" class=" event-detailsmorebtn "><a href=${roboticsDomain[0].domainName.localeCompare("ROBOTICS EVENT") == 0 ? data.eventurl : "#"} style="textdecoration:none;color:inherit" "target=_blank"}>${roboticsDomain[0].domainName.localeCompare("ROBOTICS EVENT") == 0 ? "SEE MORE" : "DETAIL COMING SOON"}</a><i style="margin-left:0.5rem;" class="fa-solid fa-arrow-right-long"></i> </button>
+                                </div>
+                            </li> 
+            `
+            eventlistUl.appendChild(newlist);
+        });
+}
+
+
+domainList.forEach((e)=>{
+    e.addEventListener('click',()=>{
+        domainDeatails.map(( domaindata )=>{
+            // console.log(domaindata)
+            let tempHeadname = domaindata.id
+            // console.log(tempHeadname)
+            if(e.innerText === tempHeadname){
+              if( idnum != domaindata.idNo ){
+                idnum=domaindata.idNo
+                if(prevDomain=="")
+                    prevDomain=domaindata.domainName;
+                console.log(prevDomain);
+                console.log(domaindata.domainName);
+                if(firstFlag)
+                {
+                    eventlistUl.innerHTML = "";
+                    firstFlag=false;
+                }
+                if(domaindata.domainName == prevDomain)
+                {
+                    console.log("Same event logging")
+                    domainFlag = true;
+                }
+                else
+                {
+                    console.log("Logging new Domain");
+                    domainFlag = false;
+                    prevDomain = domaindata.domainName;
+                    eventlistUl.innerHTML = "";
+                }
+                domaindata.eventDetails.map((data)=>{
+                    makeElement(data,domaindata.domainName) 
+                    
+                })
+            }
+            }
+        
+         })
+    })
+})
+
+
+
+const makeElement =(data,domainName)=>{
+    console.log(domainName.localeCompare("ROBOTICS EVENT"));
+    const newlist = document.createElement('li')
+    newlist.classList.add('event-eachdeatils')
+    newlist.innerHTML=`
+                    <li class="event-eachdeatils">
+                        <div class="details-heading" >${data.eventName}</div>
+                        <div class="details-description">${data.eventdescription}</div>
+                        <div class="coordinator">Coordinator: <span id="coordinator-name">${data.coordinator} (${data.coordinatorNo})</span></div>
+                        <div class="event-detailsbtndiv">
+                        <button type="button" class=" event-detailsmorebtn "> <a href=${(domainName.localeCompare("ROBOTICS EVENT")&&domainName.localeCompare("Autonomous")&&domainName.localeCompare("Coding")&&domainName.localeCompare("GAMING EVENT")) == 0 ? data.eventurl : "#"} style="textdecoration:none;color:inherit" target="_blank">${(domainName.localeCompare("ROBOTICS EVENT")&&domainName.localeCompare("Autonomous")&&domainName.localeCompare("Coding")&&domainName.localeCompare("GAMING EVENT")) == 0 ? "SEE MORE" : "DETAIL COMING SOON"}</a><i style="margin-left:"0.5rem";" class="fa-solid fa-arrow-right-long"></i> </button>
+                        </div>
+                    </li> 
+    `
+    eventlistUl.appendChild(newlist);
+
+}
+
+
+
+
+const remove = elem => {
+    console.log(elem)
+    elem.parentNode.removeChild(elem)
+}
+
+
+// console.log(domainDeatails[0])
+// console.log(domainList[0].innerText)
+
+
+
